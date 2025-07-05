61053df2c4e3dbc7b344be0afa12548c209a6594
Adityanaik4545 <adityank23@outlook.com>
2025-07-05 23:22:56 +0530

    done with nav bar

---
+++ src/components/Manager.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Manager = () => {
+  return (
+<>
+<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-700 opacity-20 blur-[100px]"></div></div> 
+<div className="container mx-auto  mycontainer">
+    <h1 className='text-center font-bold text-4xl'><span className='text-green-500'>&lt;</span>
+            Pass
+            <span className='text-green-500'>OP/&gt;</span></h1>
+    <p className='text-center text-green-900 text-lg'>Your personal password manager</p>
+
+   <div className=' flex flex-col items-center p-4 text-black gap-8'>
+        <input className='bg-white rounded-full border border-green-500 w-full p-4 py-2' type="text" />
+        <div className='flex gap-8 w-full justify-between'>
+        <input className='bg-white rounded-full border border-green-500 w-full p-4 py-2 ' type="text" />
+        <input className='bg-white rounded-full border border-green-500 w-full p-4 py-2' type="text" />
+        </div>
+        <button className='flex justify-center items-center bg-green-500 w-fit rounded-full p-2 hover:bg-green-400'>
+            <lord-icon
+    src="https://cdn.lordicon.com/gzqofmcx.json"
+    trigger="hover"
+    >
+</lord-icon>Add Password</button>
+    </div>
+</div>
+</>
+  )
+}
+
+export default Manager
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-blue-300 px-4 '>
-        <div className="logo font-bold">
-            Logo
+    <nav className=' bg-slate-600 text-white  '>
+      <div className='flex justify-between px-4 items-center py-5 mycontainer'>
+        <div className="logo font-bold text-2xl">
+          <span className='text-green-500'>&lt;</span>
+            Pass
+            <span className='text-green-500'>OP/&gt;</span>
         </div>
       <ul className='flex gap-4'>
         <a href="">Home</a>
         <a href="">Contact</a>
         <a href="">About</a>
       </ul>
+      </div>
     </nav>
   )
 }
