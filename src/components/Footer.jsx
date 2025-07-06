eee54fd3e74eacec4c638e48686e84efa86c7c0f
Adityanaik4545 <adityank23@outlook.com>
2025-07-07 01:31:10 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='w-full bg-slate-900 text-white fixed bottom-0 text-center'>
+            <div className="logo font-bold text-2xl">
+                <span className='text-green-500'>&lt;</span>
+                Pass
+                <span className='text-green-500'>OP/&gt;</span>
+            </div>
+            <div>Created by Aditya</div>
+        </footer>
+    )
+}
+
+export default Footer
